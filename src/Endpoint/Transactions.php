29b69425979eba68d3885c2cabb530ce2d709d9b
Kepsondiaz <kherabadiaby328@gmail.com>
2024-07-31 11:28:56 +0000

    add new alal's api transaction methods

--- src/Endpoint/Transactions.php
+++ src/Endpoint/Transactions.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Alal\Client\Endpoint;
 
 use Alal\Client\AlalSdk;
+use Alal\Client\Enums\BankName;
 use Alal\Client\Enums\TransctionAction;
+use Alal\Client\Enums\WalletNetwork;
 use Alal\Client\HttpClient\Message\ResponseMediator;
 
 final class Transactions
@@ -27,6 +29,26 @@ final class Transactions
     public function createTransaction( TransctionAction $action, string $card_reference, int $amount ): array
     {
         return ResponseMediator::getContent($this->sdk->getHttpClient()->post("$this->baseUri/create", [], json_encode(compact('action', 'card_reference', 'amount')))); 
+    }
+
+    public function showTransaction( string $reference = null ): array
+    {
+        return ResponseMediator::getContent($this->sdk->getHttpClient()->get("$this->baseUri/transactions")); 
+    }
+
+    public function createCardToBank( int $amount, string $card_reference, string $bank_id, BankName $bank_name, string $account_name, string $description): array
+    {
+        return ResponseMediator::getContent($this->sdk->getHttpClient()->post("$this->baseUri/transactions/create/card-to-bank", [], json_encode(compact('amount', 'card_reference', 'bank_id', 'bank_id', 'bank_name', 'account_name', 'description')))); 
+    }
+
+    public function createCardToWallet( int $amount, string $card_reference, string $phone, WalletNetwork $network): array
+    {
+        return ResponseMediator::getContent($this->sdk->getHttpClient()->post("$this->baseUri/transactions/create/card-to-wallet", [], json_encode(compact('amount', 'card_reference', 'phone', 'network')))); 
+    }
+
+    public function createWalletToCard( int $amount, string $card_reference, string $phone, WalletNetwork $network): array
+    {
+        return ResponseMediator::getContent($this->sdk->getHttpClient()->post("$this->baseUri/transactions/create/wallet-to-card", [], json_encode(compact('amount', 'card_reference', 'phone', 'network')))); 
     }
 
 }
